608a75f4a3684e14183c7cbcfbe0c3145cf83eaf
Rizwan khan <141172031+85Rizwan@users.noreply.github.com>
2024-12-04 18:39:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let sizeInfo = document.getElementById("sizeInfo")
+
+window.addEventListener("load", (e) =>{
+	sizeInfo.innerHTML = `Width: ${e.currentTarget.outerWidth} and Height: ${e.currentTarget.outerHeight}`
+	
+})
+
+window.addEventListener("resize", (e) =>{
+	sizeInfo.innerHTML = `Width: ${e.currentTarget.outerWidth} and Height: ${e.currentTarget.outerHeight}`
+	
+})
